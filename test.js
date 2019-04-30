16fe05ba88986ff67f0e7cedf675fc1f7c7e98b1
dm4 <4mylife@inbox.ru>
2019-04-30 20:28:29 +0500

    30.04

--- test.js
+++ test.js
@@ -8,9 +8,8 @@ var readlineSync = require('readline-sync');
         console.log("Обычный год");
     }  
 };
-*/ 
-
- /*var n3 = parseInt(readlineSync.question("Task 3 \nType a number "));
+*/
+/*var n3 = parseInt(readlineSync.question("Task 3 \nType a number "));
 
 function task3(n3) {
     for ( var i = 1; i < n3; i++) {
@@ -47,8 +46,23 @@ var result = '';
     }   
 }
 task5(n5);
-*/
-/* var n6 = parseInt(readlineSync.question("\nTask 6 \nType a number "));
+
+var n5a = parseInt(readlineSync.question("\nTask 5a \nType a number "));
+
+function task5a(n5a) {
+
+    
+    for( var i = 0; i < n5a; i++) {
+
+        for( var j = 1; j <= i; j++) {
+        process.stdout.write("*");
+        };
+        console.log('*');
+    }; 
+    
+};
+task5a(n5a);
+var n6 = parseInt(readlineSync.question("\nTask 6 \nType a number "));
 
 function task6(n6) {
 
@@ -63,7 +77,7 @@ function task6(n6) {
     
 };
 task6(n6);
-*/
+
  var n7 = parseInt(readlineSync.question("\nTask 7 \nType a number "));
 
 function task7(n7) {
@@ -88,19 +102,53 @@ function task7(n7) {
         }; 
         };
 task7(n7);
+*/
+/*var n8 = parseInt(readlineSync.question("\nTask 7 \nType a number "));
 
-var n5a = parseInt(readlineSync.question("\nTask 6 \nType a number "));
 
-function task5a(n5a) {
+function task8(n8) {
 
-    
-    for( var i = 0; i < n5a; i++) {
-
-        for( var j = 1; j <= i; j++) {
-        process.stdout.write("*");
+    for ( var k = 0; k < n8; k++) {
+        process.stdout.write("*")
+    }
+    for ( var j = 0; j < n8/2; j++) {
+        console.log("");
+        process.stdout.write("*")
+        for (var i = 0; i < n8/2; i++) {
+            process.stdout.write(" ");
         };
-        console.log('*');
-    }; 
-    
+        process.stdout.write("*")
+    };
+    console.log('');
+    for ( var m = 0; m <n8;m++) {
+        process.stdout.write("*")
+    };     
 };
-task5a(n5a);
+task8(n8);
+*/
+var n9 = parseInt(readlineSync.question("\nTask 8 \nType a number "));
+
+function task8(n9) {
+
+    
+    for ( var k = 0; k < n9; k++) {
+        process.stdout.write("*")
+    };
+    console.log('');
+    process.stdout.write(" ");
+    for( var i = 1; i < n9; i++) {
+        process.stdout.write("*");
+    }
+    console.log('');
+    process.stdout.write("  ");
+    for( var k = 2; k < n9; k++) {
+        process.stdout.write("*");
+    }
+    console.log('');
+    process.stdout.write("   ");
+    for( var m = 3; m < n9; m++) {
+        process.stdout.write("*");
+    };
+
+};
+task8(n9);

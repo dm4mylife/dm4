fda9caf8bb45dbe13d4baab0f103f5e9aaa00116
dm4 <4mylife@inbox.ru>
2019-05-08 12:46:10 +0500

    taskFactorialAndFibonacci

--- tasks/taskFactorial.js
+++ tasks/taskFactorial.js
@@ -1,7 +1,9 @@
+
 var readlineSync = require('readline-sync');
 
-var n = parseInt(readlineSync.question("Type a number "));
 
+var n = parseInt(readlineSync.question("Factorial in for\n Type a number "));
+console.log('');
  function factorial(n) {
 
     var current = 1;
@@ -9,21 +11,35 @@ var n = parseInt(readlineSync.question("Type a number "));
         for (var i = 1; i < n ; i++) {
             
          current = current * (i + 1) ;
-         
         };
         return current;
 };
-console.log(factorial(n));
+console.log(factorial(n))
+console.log('');
 
-var readlineSync = require('readline-sync');
 
-var n = parseInt(readlineSync.question("Task 2\n Type a number "));
+n = parseInt(readlineSync.question("Factorial in iter.process\n Type a number "));
+console.log('');
+ function factorial1(n) {
+    
+   if ( n === 1 ) {
+       return 1;
+   } else {
+       var a = n * factorial1(n-1)
+   };
+   return a;
+};
+console.log(factorial1(n));
+console.log('');
 
-a = 1;
-b = 1;
-var c = 1;
 
+n = parseInt(readlineSync.question("Task 1 Fibonacci in for \nType a number "));
+a=1;
+b=1;
+var c=1;
+console.log('');
 function fibo(n) {
+    
 for (var i = 3; i <= n && n !==1 && n !==2; i++) {
     c=a+b;
     a=b;
@@ -32,3 +48,22 @@ for (var i = 3; i <= n && n !==1 && n !==2; i++) {
 return c;
 };
 console.log(fibo(n));
+console.log('');
+
+
+n = parseInt(readlineSync.question("Task 2 Fibonacci in iter.process \nType a number "));
+a=1;
+b=1;
+var c=1;
+console.log('');
+function fibo1(n) {
+    
+if ( n === 1 || n === 2) {
+    return a;
+} else {
+    c = fibo1(n-1) + fibo1(n-2);
+}
+return c;
+};
+console.log(fibo1(n));
+console.log('');

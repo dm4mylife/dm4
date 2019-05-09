eabbbd318309cee6dfd7a0c019d4fa94e11ac74e
dm4 <4mylife@inbox.ru>
2019-05-09 22:00:28 +0500

    taskGuessANumber.js

--- tasks/taskGuessANumber.js
+++ tasks/taskGuessANumber.js
@@ -2,23 +2,25 @@ var readlineSync = require('readline-sync');
 
 function random() {
 
+    
     var max = 100;
     var min = 0;
     var random =  Math.floor(Math.random() * (max-min+1) + min); 
-    
+    console.log(random);
+    end = console.log('Повезёт в следующий раз.Наверно :)');
     for (var i = 6; i >= 0; i--) {
 
-    var n = parseInt(readlineSync.question("Type a number "));
-       
+    var n = parseInt(readlineSync.question("Type a number ")); 
     if (random > n ) {
         console.log("Загаданное число больше");
     } else if (random < n ) {
         console.log("Загаданное число меньше")
     } else if (random === n) {
-        console.log('Поздравляю,вы угадали число :)');
+        end = console.log('Поздравляю,вы угадали число :)');
+        break;
     };
     console.log(`Осталось попыток: ${i}`); 
 };
-    return console.log('Повезёт в следующий раз.Наверно :)')
+    return end;
 };
 random();

945ba20493f61ab04aba7d170a5513ea7d6ddd33
dm4 <4mylife@inbox.ru>
2019-06-01 10:57:48 +0500

    upd tell

--- tasks/tellANumber.js
+++ tasks/tellANumber.js
@@ -2,44 +2,50 @@ var readlineSync = require('readline-sync');
 
 var n = parseInt(readlineSync.question("Type a number ")); 
 
+
 function tellANumber(n) {
 
     var arr0 = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ];
     var arr1 = [ 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifthteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     var arr2 = ['twenty', 'thirty', 'fourty', 'fifthy', 'sixty', 'seventy', 'eighty', 'ninety'];
-    var arr3 = [ 'hundred' , ' thousand', 'million', 'billion'];
+    var arr3 = [ 'hundred' , 'thousand', 'million', 'billion'];
 
+    var clone = n;
     var result = '';
     var sepNum = 0;
+    var kkk = 1000000000
+    var kk = 1000000
+    var k = 1000;
     
-
-
-
-
-
+    for ( var i = 0; i < 4; i++) {
 
 
         if ( n === 0) {
 
             process.stdout.write(arr0[0]); 
+            n = Math.floor( n % 1000 );
 
         };
 
+       
         if ( n >= 100 && n <= 999 ) {
 
             var sepNum = Math.floor( n / 100 );
             
-            process.stdout.write(arr0[sepNum]+' '+arr3[0]); 
+            process.stdout.write(arr0[sepNum]+' '+arr3[0]+' '); 
 
-            if ( n % 100 !== 0 ) {
+            n = n % 100;
 
-            process.stdout.write(' and ');   
+            if ( n % 100 === 0 ) {
+
+            process.stdout.write(' and ');  
+
+            n = n - (Math.floor( n / 100) * 100);
 
             };
 
         };
            
-        n = n - (Math.floor( n / 100) * 100);
          
         if (  n > 20 && n < 100  ) {
 
@@ -47,7 +53,7 @@ function tellANumber(n) {
             
             process.stdout.write(arr2[sepNum]);
 
-        n = n % 10;   
+            n = n % 10;   
 
         };
 
@@ -59,14 +65,55 @@ function tellANumber(n) {
 
         };
 
-        if ( n <= 9 && n !== 0) {
+        if ( n <= 9 ) {
 
+        sepNum = n;
         process.stdout.write(arr0[sepNum]);
             
         };
+
+        console.log(clone)
+        if ( ( clone / kkk ) > 1 ) {
+    
+            sepNum = Math.floor( n / kkk );
+            process.stdout.write(arr0[sepNum]+arr3[3]+' and ');
+            n = Math.floor( n / k ) % k ;
+            clone = Math.floor( n / 1000 );
+            
         
-        return result;
-};
+          } else if ( ( clone / kk ) > 1) {
+  
+            process.stdout.write(arr3[2]+' and ');
+            n = Math.floor( n / k ) % k ;
+            clone = Math.floor( n / 1000 );
+            i = i +1;
+  
+          } else if  ( clone / k === 1 ) {
+
+            sepNum = Math.floor( n / k );
+            process.stdout.write(arr0[sepNum]+arr3[1]);
+            i = i + 3;
+
+        } else if ( ( clone / k ) > 1 ) {
+
+            
+            sepNum = Math.floor( n / k );
+            process.stdout.write(arr0[sepNum]+' '+arr3[1]+' and ');
+            n =  n % k  ;
+            clone =  n % k;
+            i = i + 2;
+
+          } else {
+
+              i = i + 4;
+
+          };
+
+    };
+
+    };
+   
+
 tellANumber();
 
 console.log(tellANumber(n));
--- tasks/test.js
+++ tasks/test.js
@@ -1,16 +1,38 @@
 
-var n =5
 
-var m =5;
-var result =1;
+var n = 1;
 
-for ( var i = 1 ; i < n; i++) {
-var k = i % m;
-result = result * k + 2;
-console.log(result)
+var kkk = 1000000000
+var kk = 1000000
+var k = 1000;
+var clone = n;
 
-}
+if ( ( clone / kkk ) > 0 ) {
+    
+  sepNum = Math.floor( n / kkk );
+  process.stdout.write(arr0[sepNum]+arr3[3]+' and ');
+  n = Math.floor( n / k ) % k ;
+  clone = Math.floor( n / 1000 );
+  
 
+} else if ( ( clone / kk ) > 0) {
 
+  process.stdout.write(arr3[2]+' and ');
+  n = Math.floor( n / k ) % k ;
+  clone = Math.floor( n / 1000 );
+  i = i +1;
 
-  console.log(result)  
+} else if ( ( clone / k ) > 0 ) {
+
+  process.stdout.write(arr3[1]+' and ');
+  n = Math.floor( n / k ) % k ;
+  clone = Math.floor( n / 1000 )
+  i = i + 2;
+
+} else {
+
+    i = i + 4;
+
+};
+console.log(n)
+  

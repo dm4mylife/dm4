26902ae36ba6d47e31f2b90249b9ad963ef44226
dm4 <4mylife@inbox.ru>
2019-06-05 08:49:53 +0500

    upd

--- tasks/tellANumber.js
+++ tasks/tellANumber.js
@@ -7,141 +7,91 @@ function tellANumber(n) {
     var arr0 = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ];
     var arr1 = [ 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifthteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     var arr2 = ['twenty', 'thirty', 'fourty', 'fifthy', 'sixty', 'seventy', 'eighty', 'ninety'];
-    var arr3 = [ 'hundred' , 'thousand', 'million', 'billion'];
+    var arr3 = [ 'billion','million','thousand','hundred' ];
 
-    var clone = n;
-    var result = '';
-    var sepNum = 0;
-    var kkk = 1000000000;
-    var kk = 1000000;
-    var k = 1000;
+    var k = 1000000000;
 
 
     if ( n < 0 ) {
 
         process.stdout.write('minus ');
         n = n * -1;
+
+    };
+
+    if ( n === 0 ) {
+
+        process.stdout.write(arr0[0]);
+    };
+
+     for ( var i = 0; i < 3; i++ ) {
+
+        var a = Math.floor( n / k );
+
+        if ( a != 0 ) {
+
+            triple(a);
+            process.stdout.write(' '+ arr3[i]);
+
+        };
+
+    k = Math.floor( k / 1000 );
+
     };
     
-    for ( var i = 0; i < 4; i++ ) {
-
-        if ( n === 0 && i === 0) { 
-
-            process.stdout.write(arr0[0]+' '); 
-            i = 4;
-            clone = -1;
-        };
-
-        if ( ( clone / kkk ) > 1 ) {
-    
-            clone = n % kkk;
-            n = Math.floor( n / kkk );
-            
-          }  else if  ( clone % kk === 0 ) {
-            clone = n % kk;
-            sepNum = Math.floor( n / kk );
-            process.stdout.write(arr0[sepNum]+' '+arr3[2]);
-            i = 3;
-          
-          } else if ( ( clone / kk ) > 1 ) {
-
-            clone = n % kk;
-            n = Math.floor( n / kk );
-            i =  1;
-  
-          } else if  ( clone % k === 0 ) {
-            
-            n = Math.floor( n / k  );
-            clone = n % k;
-            i = 3;
-
-        } else if ( ( clone / k ) > 1 ) {
-
-            clone = n % k ;
-            n =  Math.floor( n / k );
-            i = 2;
-            
-          } else {
-
-              i = 4;
-
-          };
-         
-       
-        if ( n >= 100 && n <= 999 ) {
-
-            var sepNum = Math.floor( n / 100 );
-            
-            process.stdout.write(arr0[sepNum]+' '+arr3[0]+' '+' and '); 
-
-            n = n % 100;
-
-            if ( n % 100 === 1 ) {
-
-            process.stdout.write(' and ');  
-
-            n = n - (Math.floor( n / 100) * 100);
-
-            };
-
-        };
-           
-         
-        if ( n >= 20 && n < 100 ) {
-
-            sepNum = Math.floor( n / 10 ) - 2;
-            
-            process.stdout.write(arr2[sepNum]);
-
-            n = n % 10;   
-
-        };
-
-        sepNum = n % 10;
-
-        if ( n >= 10 && n < 20 ) {
-        
-        process.stdout.write(arr1[sepNum]);
-
-        };
-
-        if ( n > 0 && n <= 9 ) {
-
-        sepNum = n;
-        process.stdout.write(arr0[sepNum]+' ');
-            
-        };
-
-        if ( i  === 2 ) {
-
-            process.stdout.write(arr3[1]+' and '); 
-            n = clone;
-        
-        }; 
-
-        if ( i === 1 ) {
-
-            process.stdout.write(arr3[2]+' and '); 
-            n = clone;
-
-        };
-
-        if ( i === 0 ) {
-
-            process.stdout.write(arr3[3]+' and ');
-            n = clone
-            
-        };
-        if ( i === 3 ) {
-            process.stdout.write(' '+arr3[1]);
-
-        }
+    triple(n);
 
     };
-
-    };
-   
 
 tellANumber();
 
 console.log(tellANumber(n));
+
+function triple() {
+           
+    var arr0 = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ];
+    var arr1 = [ 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifthteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    var arr2 = ['twenty', 'thirty', 'fourty', 'fifthy', 'sixty', 'seventy', 'eighty', 'ninety'];
+    var arr3 = [ 'hundred' , 'thousand', 'million', 'billion'];
+
+    var sepNum = 0;
+    var kkk = 1000000000;
+    var kk = 1000000;
+    var k = 1000;
+
+    if ( n >= 100 && n <= 999 ) {
+
+        var sepNum = Math.floor( n / 100 );
+        
+        process.stdout.write(arr0[sepNum]+' '+arr3[0]+' '+' and '); 
+
+        n = n % 100;
+
+    };
+    
+    
+    if ( n >= 20 && n < 100 ) {
+
+        sepNum = Math.floor( n / 10 ) - 2;
+        
+        process.stdout.write(arr2[sepNum]);
+
+        n = n % 10;   
+
+    };
+
+    sepNum = n % 10;
+
+    if ( n >= 10 && n < 20 ) {
+    
+    process.stdout.write(arr1[sepNum]);
+
+    };
+
+    if ( n > 0 && n <= 9 ) {
+
+    sepNum = n;
+    process.stdout.write(arr0[sepNum]+' ');
+        
+    };
+};

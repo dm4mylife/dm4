af09aa894bcc97d4853fcbd855628cb6821078e6
dm4 <4mylife@inbox.ru>
2019-06-05 11:43:11 +0500

    ready tellANumber

--- tasks/tellANumber.js
+++ tasks/tellANumber.js
@@ -4,50 +4,48 @@ var n = parseInt(readlineSync.question("Type a number "));
 
 function tellANumber(n) {
 
-    var arr0 = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ];
-    var arr1 = [ 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifthteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    var arr2 = ['twenty', 'thirty', 'fourty', 'fifthy', 'sixty', 'seventy', 'eighty', 'ninety'];
-    var arr3 = [ 'billion','million','thousand','hundred' ];
+    var arr3 = [ 'billion','million','thousand' ];
 
     var k = 1000000000;
+    
+    if ( n <= 4 * k && n >= -4 * k ) { 
 
+        if ( n < 0 ) {
 
-    if ( n < 0 ) {
-
-        process.stdout.write('minus ');
-        n = n * -1;
-
-    };
-
-    if ( n === 0 ) {
-
-        process.stdout.write(arr0[0]);
-    };
-
-     for ( var i = 0; i < 3; i++ ) {
-
-        var a = Math.floor( n / k );
-
-        if ( a != 0 ) {
-
-            triple(a);
-            process.stdout.write(' '+ arr3[i]);
+            process.stdout.write('minus ');
+            n = n * -1;
 
         };
 
-    k = Math.floor( k / 1000 );
+        if ( n === 0 ) {
 
+            process.stdout.write('zero');
+        };
+
+
+        for ( var i = 0; i < 3; i++ ) {
+
+            var a = Math.floor( n / k );
+            
+            if ( a !== 0 ) {
+                
+                triple(a);
+
+                process.stdout.write(' '+ arr3[i]+' ');
+                n = n - (a * k); 
+            }; 
+            
+        k =  k / 1000;
+
+        };
+        
+        triple(n); 
     };
+};
     
-    triple(n);
+tellANumber(n);
 
-    };
-
-tellANumber();
-
-console.log(tellANumber(n));
-
-function triple() {
+function triple(a) {
            
     var arr0 = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ];
     var arr1 = [ 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifthteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
@@ -55,43 +53,38 @@ function triple() {
     var arr3 = [ 'hundred' , 'thousand', 'million', 'billion'];
 
     var sepNum = 0;
-    var kkk = 1000000000;
-    var kk = 1000000;
-    var k = 1000;
 
-    if ( n >= 100 && n <= 999 ) {
+    if ( a >= 100 && a <= 999 ) {
 
-        var sepNum = Math.floor( n / 100 );
+        var sepNum = Math.floor( a / 100 );
         
-        process.stdout.write(arr0[sepNum]+' '+arr3[0]+' '+' and '); 
+        process.stdout.write(arr0[sepNum]+' '+arr3[0]+" "); 
 
-        n = n % 100;
+        a = a % 100;
 
     };
     
-    
-    if ( n >= 20 && n < 100 ) {
+    if ( a >= 20 && a < 100 ) {
 
-        sepNum = Math.floor( n / 10 ) - 2;
+        sepNum = Math.floor( a / 10 ) - 2;
         
         process.stdout.write(arr2[sepNum]);
 
-        n = n % 10;   
+        a = a % 10;   
 
     };
 
-    sepNum = n % 10;
+    sepNum = a % 10;
 
-    if ( n >= 10 && n < 20 ) {
+    if ( a >= 10 && a < 20 ) {
     
     process.stdout.write(arr1[sepNum]);
 
     };
 
-    if ( n > 0 && n <= 9 ) {
+    if ( a > 0 && a <= 9 ) {
 
-    sepNum = n;
-    process.stdout.write(arr0[sepNum]+' ');
+    process.stdout.write(arr0[a]+' ');
         
     };
-};
+}; 
--- tasks/test.js
+++ tasks/test.js
@@ -1,38 +1,15 @@
 
+var m = 0;
+n = 1000000000;
+k = 1000000000;
+var a = Math.floor(n / k);
+console.log(a);
+if ( a != 0 ) {
 
-var n = 1;
-
-var kkk = 1000000000
-var kk = 1000000
-var k = 1000;
-var clone = n;
-
-if ( ( clone / kkk ) > 0 ) {
-    
-  sepNum = Math.floor( n / kkk );
-  process.stdout.write(arr0[sepNum]+arr3[3]+' and ');
-  n = Math.floor( n / k ) % k ;
-  clone = Math.floor( n / 1000 );
-  
-
-} else if ( ( clone / kk ) > 0) {
-
-  process.stdout.write(arr3[2]+' and ');
-  n = Math.floor( n / k ) % k ;
-  clone = Math.floor( n / 1000 );
-  i = i +1;
-
-} else if ( ( clone / k ) > 0 ) {
-
-  process.stdout.write(arr3[1]+' and ');
-  n = Math.floor( n / k ) % k ;
-  clone = Math.floor( n / 1000 )
-  i = i + 2;
+  console.log('Проверка прошла');
 
 } else {
 
-    i = i + 4;
+console.log('Проверка не прошла');
 
 };
-console.log(n)
-  

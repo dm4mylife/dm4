c1c7188b5d570d9b0ac16baf8ec9357d4ed62341
dm4 <4mylife@inbox.ru>
2019-06-27 21:10:47 +0500

    upd

--- tasks/ticTakToe.js
+++ tasks/ticTakToe.js
@@ -62,15 +62,60 @@ function game() {
     var char = 'X '
     var endGame = false;
 
-        for ( var i = 0; i < 9; i++ ) {
+        for ( var k = 0; k < 9; k++ ) {
 
-    
-            var y = parseInt(readlineSync.question("Player:"+player+"  Type Y\n"));
-            var x = parseInt(readlineSync.question("Player:"+player+"  Type X \n"));
+            console.log(k)
 
-            result1[y][x] = char;
-            tictactoe(result1);
+            function isCorrect(y,x) {
 
+
+                            while( ( y > 3 || y < 1 ) || ( x > 3 || x < 1 ) ) {
+
+                                console.log('Wrong number, type again');
+                                y = parseInt(readlineSync.question("Player:"+player+"  Type Y\n"));
+                                x = parseInt(readlineSync.question("Player:"+player+"  Type X \n"));
+                            }; 
+                                
+                            var arrN = {"y":y,'x':x}
+                            
+                            return arrN;
+
+            };
+
+            function isEmptyField(y,x,result1) {
+
+
+                                    while ( result1[y-1][x-1] === 'X ' || result1[y-1][x-1] === 'O ') {
+
+                                        console.log("Field is full, type again");
+
+                                        y = parseInt(readlineSync.question("Player:"+player+"  Type Y\n"));
+                                        x = parseInt(readlineSync.question("Player:"+player+"  Type X \n"));
+                                        
+                                    };
+                                    
+                                    var arrN = {"y":y,"x":x}
+                                    
+                                    return arrN;
+                                    
+
+            };
+                
+                var y = parseInt(readlineSync.question("Player:"+player+"  Type Y\n"));
+                var x = parseInt(readlineSync.question("Player:"+player+"  Type X \n"));
+
+                var i = isCorrect(y,x);
+                y = i.y;
+                x = i.x;
+
+                var p = isEmptyField(y,x,result1);
+                y = p.y;
+                x = p.x;
+
+                result1[y-1][x-1] = char;
+                tictactoe(result1);
+
+                
             if ( (result1[0][0] === 'X ' && result1[0][1] === 'X ' && result1[0][2] === 'X ') || 
                 ( result1[1][0] === 'X ' && result1[1][1] === 'X ' && result1[1][2] === 'X ') || 
                 ( result1[2][0] === 'X ' && result1[2][1] === 'X ' && result1[2][2] === 'X ') || 
@@ -84,6 +129,7 @@ function game() {
                 endGame = true;
 
             };
+            
 
             if ( (result1[0][0] === 'O ' && result1[0][1] === 'O ' && result1[0][2] === 'O ') || 
                 ( result1[1][0] === 'O ' && result1[1][1] === 'O ' && result1[1][2] === 'O ') || 

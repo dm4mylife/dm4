90016f2b1c7f1d91bf79458cc0c05e3ef500e24b
dm4 <4mylife@inbox.ru>
2019-08-21 17:24:01 +0500

    upd

--- final/index.js
+++ final/index.js
@@ -1,15 +1,5 @@
-
-
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d')
-
-
-
-/* if ( x === 1100 ) {
-    x = 0;
-} else {
-    x += 110;
-}; */
 
 var pictures  = [];
 
@@ -20,81 +10,96 @@ for ( var i = 1; i <= 12; i++ ) {
     pictures.push(sprite);
 
 }
-console.log(pictures)
-var sprite = new Image('');
-sprite.src = '1.png';
-
+var jumpPressed = false;
+var dy = 0;
  var godzilla = {
 
     frame_id : 0,
-    y_pos : 50,
+    y_pos : 80,
     time : 0
 
-    }
-var arr = [1,2,3,4]
-console.log(arr[-2])
-if ( 'abc' > 'df'){
-    console.log('true')}
-else {
-    console.log('nj')
-}
-/* function drawGodzilla(pictures) {
+    };
 
-    for ( var i = 1; i <= 6; i++ ) {
-
-
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-
-        ctx.drawImage(pictures[i],0,60);
-
-    }; 
-
-    drawGodzilla(pictures) 
-
-    };*/
-time = 0;
 frame_id = 0;
 
 function moveGodzilla() {
 
     ctx.clearRect(0,0,canvas.width,canvas.height);
 
-if ( godzilla.time > 1000) {
+    if ( godzilla.time > 1000) {
 
-        godzilla.time = 0;
-    }
+            godzilla.time = 0;
+            time = 0;
+            
+        }
 
     frame_id = Math.floor(godzilla.time / 1000 * pictures.length);
-    console.log(frame_id)
-
-    ctx.drawImage(pictures[frame_id],0,godzilla.y_pos,50,70);
+   
+    ctx.drawImage(pictures[frame_id],5,godzilla.y_pos,40,60);
     
     godzilla.time +=30;
-
     
 
-};
+    if ( jumpPressed ) {
+
+        jump();
+
+    };
+
+    function jump() {
+        
+        godzilla.y_pos += dy;
+    
+        if ( godzilla.y_pos > 5 ) {
+    
+            dy -= 1;
+    
+        } else {
+    
+             dy += 1;
+
+        if( dy === 0 ) {
+
+            godzilla.y_pos = 80;
+            jumpPressed = false;
+
+        }
+             
+    
+         }
+    };
+
+}
+
+var timer; 
+           
+        
+    document.onclick = function () { 
+
+        timer = setInterval(moveGodzilla, 20); 
+        console.log('Run');
+
+    };
+
+    document.oncontextmenu = function (event) {
+        
+            clearInterval(timer);
+            return false;
+    }
+
+    var dy = 0; 
+
+    document.onkeydown = function (event) {
+    console.log(event)
+
+    if ( event.keyCode === 38 ) {
+
+            console.log('Pressed jump');
+            jumpPressed = true;
             
-        
-setInterval(moveGodzilla, 30);
-    
-document.onkeydown = function (event) {
-   
-
-   if ( event.keyCode === 38 ) {
-
-        console.log(event.keyCode)
-        
-        console.log(godzilla.y_pos)
-
-
-        godzilla.y_pos -= 1;
-
 
     }
-          
-
+            
         
-
 };
         

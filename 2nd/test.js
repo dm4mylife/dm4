38641f3afe2378ed28e77fc585f5cabf5fce2570
dm4mylife <4mylife.nb@gmail.com>
2019-11-07 20:06:35 +0500

    upd

--- 2nd/test.js
+++ 2nd/test.js
@@ -3,7 +3,6 @@ class ItemProps {
     constructor() {
 
         this.health = 10;
-        this.mana = 10;
         this.damage = 2;
         this.defence = 2;
 
@@ -16,76 +15,144 @@ var shineHelmet = new ItemProps();
 shineHelmet.defence = 5;
 shineHelmet.health = 20;
 
-var fastGloves = new ItemProps();
-
-fastGloves.damage = 4;
-fastGloves.defence = 3;
-
-var chaosSword = new ItemProps();
-
-chaosSword.damage = 5;
-chaosSword.health = 30;
-
 var roughChest = new ItemProps();
 
 roughChest.health = 30;
 roughChest.defence = 4;
 
+var spikeShoulders = new ItemProps();
+
+spikeShoulders.health = 30;
+spikeShoulders.defence = 4;
+
+var fastGloves = new ItemProps();
+
+fastGloves.health = 14;
+fastGloves.defence = 3;
+
+var chaosSword = new ItemProps();
+
+chaosSword.damage = 5;
+
+
+var travelBoots = new ItemProps();
+
+travelBoots.damage = 3;
+travelBoots.health = 10;
+travelBoots.defence = 1;
 
 class Creature {
 
     constructor() {
 
-        this.health = 100;
-        this.damage = 10
-        this.mana = 100;
+        this.name = '';
+        this.health = 1000;
+        this.damage = 60
         this.defence = 5;
         this.helmet = null;
-        this.shoulders = null;
         this.chest = null;
+        this.shoulders = null;
         this.gloves = null;
         this.pants = null;
         this.boots = null;
+        this.sword = null;
+
 
     };
+
     battle(enemy) {
-        if (enemy.damage > this.damage) {
-            console.log('+')
+
+
+        var defenceStatsAxis = 0;
+
+        if ( enemy.helmet != null) {
+
+            defenceStatsAxis += enemy.helmet.defence;
+
+        } else if ( enemy.shoulders != null) {
+
+            defenceStatsAxis += enemy.shoulders.defence;
+
+        } else if ( enemy.chest != null) {
+
+            defenceStatsAxis += enemy.chest.defence;
+
+        } else if ( enemy.gloves != null) {
+
+            defenceStatsAxis += enemy.gloves.defence;
+
+        } else if ( enemy.pants != null) {
+
+            defenceStatsAxis += enemy.pants.defence;
+
+        } else if ( enemy.boots != null) {
+
+            defenceStatsAxis += enemy.boots.defence;
+
+        };
+
+        defenceStatsAxis = enemy.defence + enemy.helmet.defence + enemy.shoulders.defence+ enemy.chest.defence+ enemy.pants.defence+ enemy.gloves.defence+ enemy.boots.defence;
+
+        var damageStatsAlies = this.damage + this.sword.damage;
+
+        console.log(defenceStatsAxis,enemy)
+
+        if ( defenceStatsAxis > 10 ) {
+
+            enemy.health -= damageStatsAlies - defenceStatsAxis - 5 ;
+            
+        } else if ( defenceStatsAxis > 7 ) {
+
+            enemy.health -= damageStatsAlies - defenceStatsAxis - 1 ;
+            
+        } else if ( defenceStatsAxis > 5 ) {
+
+            enemy.health -= damageStatsAlies - defenceStatsAxis - 1 ;
+            
         } else {
-            console.log('-')
-        }
-    }
 
+            enemy.health -= damageStatsAlies - defenceStatsAxis;
 
+        };
+
+       
+        damageStatsAlies -= .5;
+        enemy.health -= damageStatsAlies;
+        
+    };
 };
-
 
 var hollyKnight = new Creature();
+
+hollyKnight.name = 'HollyKnight';
 hollyKnight.helmet = shineHelmet;
-hollyKnight.def = 5;
+hollyKnight.chest = roughChest;
+hollyKnight.gloves = fastGloves;
+
 var ghoul = new Creature();
+
+ghoul.name = 'Ghoul';
 ghoul.chest = roughChest;
-ghoul.
+ghoul.sword = chaosSword;
+ghoul.boots = travelBoots;
 
-ghoul.battle(hollyKnight);
-hollyKnight.battle(hollyKnight);
+while ( ghoul.health > 0 || hollyKnight.health > 0) {
 
-class Enemy {
+    hollyKnight.battle(ghoul);
+    ghoul.battle(hollyKnight);
 
-    constructor() {
-
-        this.health = 80;
-        this.damage = 7
-        this.mana = 40;
-        this.defence = 3;
-        this.strenghtPoints = 8;
-        this.agilityPoints = 5;
-        this.intelligence = 3;
-        this.spellHollyStrike = false;
-    };
+    printStats(hollyKnight);
+    printStats(ghoul);
 
 };
 
-var ghoul = new Enemy();
+function printStats(unit) {
 
-hollyKnight.battle(ghoul)
+    console.log(`Stats ${unit.name}:\n${unit.health} ---health\n${unit.defence} ---defence\n${unit.damage} --- damage`);
+    
+     if (unit.health < 0) {
+
+         return console.log('Winner is '+ unit.name);
+
+     };
+};
